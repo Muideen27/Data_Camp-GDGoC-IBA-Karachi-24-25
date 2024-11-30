1db41048b8723912fae706a0af6200bbf5b124ae
Muideen27 <ilorimuideen0000@gmail.com>
2024-11-30 20:49:14 +0100

    Variable assignment 3

---
+++ Intriduction-to-R/4-script.R
@@ -0,0 +1,14 @@
+# Assign to my_oranges the value 6.
+# Add the variables my_apples and my_oranges and have R simply print the result.
+# Assign the result of adding my_apples and my_oranges to a new variable my_fruit.
+
+# Assign a value to the variables my_apples and my_oranges
+my_apples <- my_oranges <- 5
+
+
+# Add these two variables together
+my_apples + my_oranges
+
+# Create the variable my_fruit
+my_fruit <- my_apples + my_oranges
+print(my_fruit)
