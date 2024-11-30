407855c166beeb5391a6bc71048be02f0875455e
Muideen27 <ilorimuideen0000@gmail.com>
2024-11-30 21:01:34 +0100

    Create a vector 3

---
+++ Intriduction-to-R/10-script.R
@@ -0,0 +1,11 @@
+# Assign the winnings/losses for roulette to the variable 
+# roulette_vector. You lost $24, then lost $50, won $100, 
+# lost $350, and won $10.
+
+# Poker winnings from Monday to Friday
+poker_vector <- c(140, -50, 20, -120, 240)
+print(poker_vector)
+
+# Roulette winnings from Monday to Friday
+roulette_vector <- c(-24, -50, 100, -350, 10)
+print(roulette_vector)
