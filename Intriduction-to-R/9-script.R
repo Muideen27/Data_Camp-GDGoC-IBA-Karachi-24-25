3215fe19174854ca2da620a39a1cd940101698c8
Muideen27 <ilorimuideen0000@gmail.com>
2024-11-30 20:58:24 +0100

    Create a vector (2)

---
+++ Intriduction-to-R/9-script.R
@@ -0,0 +1,7 @@
+# Complete the code such that boolean_vector contains the three elements: TRUE, FALSE and TRUE (in that order).
+
+numeric_vector <- c(1, 10, 49)
+character_vector <- c("a", "b", "c")
+
+# Complete the code for boolean_vector
+boolean_vector <- c(TRUE, FALSE, TRUE)
