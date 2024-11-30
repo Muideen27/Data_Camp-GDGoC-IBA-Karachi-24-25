42ecf57bdb0ffe4702576719c0a013691cb9cfda
Muideen27 <ilorimuideen0000@gmail.com>
2024-11-30 20:42:27 +0100

    Variable Assignment

---
+++ Intriduction-to-R/2-script.R
@@ -0,0 +1,9 @@
+# complete the code in the editor such that it assigns the value 42 
+# to the variable x in the editor. Submit the answer. 
+# Notice that when you ask R to print x, the value 42 appears.
+
+# Assign the value 42 to x
+x <- 42
+
+# Print out the value of the variable x
+print(x)
