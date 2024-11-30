ccace0584a38a69dbf98563710bc8e00725d8fe5
Muideen27 <ilorimuideen0000@gmail.com>
2024-11-30 20:39:57 +0100

    Arithmetics with R

---
+++ Intriduction-to-R/1-script.R
@@ -0,0 +1,22 @@
+# Type 2^5 in the editor to calculate 2 to the power 5.
+# Type 28 %% 6 to calculate 28 modulo 6.
+# Submit the answer and have a look at the R output in the console.
+# Note how the # symbol is used to add comments on the R code.
+
+# An addition
+5 + 5 
+10
+# A subtraction
+5 - 5 
+0
+# A multiplication
+3 * 5
+
+ # A division
+(5 + 5) / 2 
+5
+# Exponentiation
+5 ^ 3
+125
+# Modulo
+5 %% 3
