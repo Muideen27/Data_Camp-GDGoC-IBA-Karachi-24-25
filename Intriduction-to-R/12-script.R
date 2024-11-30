41650d335d2467b83b840f3aab120b15f51bd7ea
Muideen27 <ilorimuideen0000@gmail.com>
2024-11-30 21:04:55 +0100

    Naming a vector 2

---
+++ Intriduction-to-R/12-script.R
@@ -0,0 +1,15 @@
+A variable days_vector that contains the days of the week has already been created for you.
+Use days_vector to set the names of poker_vector and roulette_vector.
+
+# Poker winnings from Monday to Friday
+poker_vector <- c(140, -50, 20, -120, 240)
+
+# Roulette winnings from Monday to Friday
+roulette_vector <- c(-24, -50, 100, -350, 10)
+
+# The variable days_vector
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+ 
+# Assign the names of the day to roulette_vector and poker_vector
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
