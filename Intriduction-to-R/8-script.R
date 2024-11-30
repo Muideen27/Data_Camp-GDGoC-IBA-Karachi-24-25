ef95d76257bc8a96bdbcdc5270036ec9c8986127
Muideen27 <ilorimuideen0000@gmail.com>
2024-11-30 20:56:15 +0100

    Create a vector

---
+++ Intriduction-to-R/8-script.R
@@ -0,0 +1,5 @@
+# Do you still remember what you have learned in the first chapter? Assign the value "Go!" to the variable vegas. Remember: R is case sensitive!
+
+# Define the variable vegas
+vegas <- "Go!"
+print(vegas)
