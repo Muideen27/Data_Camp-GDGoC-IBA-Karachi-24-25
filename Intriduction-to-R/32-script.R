73e2d8a3f59d87d395c62b0a671ee41e55989df1
Muideen27 <ilorimuideen0000@gmail.com>
2024-12-17 00:11:07 +0100

    Little Arithmetic with matric

---
+++ Intriduction-to-R/32-script.R
@@ -0,0 +1,9 @@
+# Assume an average ticket price (in millions)
+ticket_price <- 5
+
+# Estimate the visitors by dividing the revenue by the ticket price
+visitors <- all_wars_matrix / ticket_price
+
+# Print the visitors to the console
+print(visitors)
+
