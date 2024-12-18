0f53270cc04065fc54783602b8a60c6142207aba
Muideen27 <ilorimuideen0000@gmail.com>
2024-12-18 23:24:53 +0100

    Greater and less than

---
+++ Intermediate-R/1-script.R
@@ -0,0 +1,15 @@
+# Write R expressions to check whether:
+
+# -6 * 5 + 2 is greater than or equal to -10 + 1.
+#"raining" is less than or equal to "raining dogs".
+# TRUE is greater than FALSE.
+
+# Comparison of numerics
+-6 * 5 + 2 >= -10 + 1
+
+# Comparison of character strings
+"raining" <= "raining dogs"
+
+# Comparison of logicals
+TRUE > FALSE
+
