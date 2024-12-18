6bd67d4452caea4d7bb6f563a2d37eab70bf87c4
Muideen27 <ilorimuideen0000@gmail.com>
2024-12-18 23:17:31 +0100

    Equality

---
+++ Intermediate-R/0-script.R
@@ -0,0 +1,16 @@
+# In the editor on the right, write R code to see if TRUE equals FALSE.
+# Likewise, check if -6 * 14 is not equal to 17 - 101.
+# Next up: comparison of character strings. Ask R whether the strings "useR" and "user" are equal.
+# Finally, find out what happens if you compare logicals to numerics: are TRUE and 1 equal?
+
+# Comparison of logicals
+TRUE == FALSE
+
+# Comparison of numerics
+(-6 * 14) != (17 - 101)
+
+# Comparison of character strings
+"useR" == "user"
+
+# Compare a logical with a numeric
+TRUE == 1
